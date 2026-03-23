2c8874acf32e44cf6e2dfe88ded1594708304519
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-23 18:52:24 +0530

    feat: Implemented evalPING

--- core/eval.go
+++ core/eval.go
@@ -1,7 +1,29 @@
+
+func evalPING(args []string,c net.Conn)error{
+	var b []byte 
+
+	if len(args)>=2{
+		//means if the redis cli passes us more than 1 arguments then this will invoke 
+		return errors.New("ERR wrong number of arguments for 'ping' command")
+	}
+
+	if len(args)==0{
+		//we will encode it into RESP
+		//encode function is to take the raw type and convert it to another encoded resp format
+		//Because server has to respond in resp format so the server will also do get the thing 
+		b=Encode("PONG",true)
+	}else{
+		b=Encode(args[0],false)
+	}
+
+	_,err:=c.Write(b)
+	return err 
+}
+
 func EvalAndRespond(cmd *Rediscmd,c net.Conn)error{
 	//It's job is like depending on what job is sent to us
 	//we trigger the corresponding eval function
-	
+
 	switch cmd.Cmd{
 	case "PING":
 		return evalPING(cmd.Args,c)
