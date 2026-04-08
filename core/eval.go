35de7956133746d5a368489e50a17428863b37c9
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-09 03:43:33 +0530

    feat: Added more features like get,set,ttl

--- core/eval.go
+++ core/eval.go
@@ -33,6 +33,12 @@ func EvalAndRespond(cmd *RedisCmd, c io.ReadWriter) error{
 	switch cmd.Cmd{
 	case "PING":
 		return evalPING(cmd.Args,c)
+	case "SET":
+		return evalSET(cmd.Args,c)
+	case "GET":
+		return evalGET(cmd.Args,c)
+	case "TTL":
+		return evalTTL(cmd.Args,c)
 	default:
 		return evalPING(cmd.Args,c);
 	}
