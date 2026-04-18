ca4a02d5df32f9066c8620689f3c8bf8823c0165
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-18 12:34:02 +0530

    feat: Implemented evictallkeysrandom

--- core/eviction.go
+++ core/eviction.go
@@ -7,6 +7,8 @@ whenevr a cache is full, we will be evicting the first key which we do find
 
 package core
 
+import "github.com/sharpsalt/Velox-In-Memory-Database/config"
+
 //Evcits the first key it found while iterating the map 
 //TODP: Make it efficient by doing thrugh somehting
 func evictFirst(){
@@ -16,9 +18,36 @@ func evictFirst(){
 	}
 }
 
+//Randomly removes keys to make space for the new data added
+//The number of keys removed will be sufficient to free up least 10% space
+func evictAllKeysRandom(){
+	evictCount:=int64(config.EvictionRatio*float64(config.KeysLimit))
+	//Iteration of Golang dictionary can be considered as a random
+	//because it depends on the has of the inserted key 
+	for k:= range store{
+		Del(k)
+		evictCount--
+		if evictCount==0{
+			break
+		}
+	}
+}
+/*
+How do we know that our eviction is working correctly??
+
+which means when we are doing large number of key sets , we are putting lots of keys in db , let's say we put kelimit to 100
+, how do we sure that we are not breachng it , so every dbs supports statistics
+*/
+
 
 //TODO: Make the eveiction strategy configuration dirven
 //TODO: Support multiple eviction strategies
 func evict(){
-	evictFirst()
+	// evictFirst()
+	switch config.EvictionStrategy{
+	case "simple-first":
+		evictfirst()
+	case "allkets-random":
+		evictAllkeysRandom()
+	}
 }
