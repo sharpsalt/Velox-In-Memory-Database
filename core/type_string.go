37b879ed70e3a8c7b741f35eb6dc089fefc5205d
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-24 00:32:18 +0530

    fix: fixed syntax issue

--- core/type_string.go
+++ core/type_string.go
@@ -7,7 +7,7 @@ import "strconv"
 //tryObjectEncoding Function in redis
 func deduceTypeEncoding(v string)(uint8,uint8){
 	oType:=OBJ_TYPE_STRING
-	if _,rr:=strconv.ParseInt(v,10,64);err==nil{
+	if _,err:=strconv.ParseInt(v,10,64);err==nil{
 		return oType,OBJ_ENCODING_INT //we are converting ans stroing int as a string inside my redis object
 	}
 	if len(v)<=44{
