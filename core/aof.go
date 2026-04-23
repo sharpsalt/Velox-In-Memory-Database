41dbb9b2fee2c9f3b645b0346566ed38db544701
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-24 00:24:28 +0530

    fix: splitted string by space, earlier had no space

--- core/aof.go
+++ core/aof.go
@@ -13,7 +13,7 @@ import (
 //TODO: Support sync write
 func dumpKey(fp *os.File,key string,obj *Obj){
 	cmd:=fmt.Sprintf("SET %s %s", key,obj.Value)
-	tokens:=strings.Split(cmd,"")
+	tokens:=strings.Split(cmd," ")
 	fp.Write(Encode(tokens,false))
 }
 
