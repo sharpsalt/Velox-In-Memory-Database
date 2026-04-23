6f55d55ec39538e50e799b78082b3593a1cd6ad2
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-23 06:07:59 +0530

    feat: replaced ExpireAt with LastAccessedAt

--- core/object.go
+++ core/object.go
@@ -4,8 +4,21 @@ package core
 //TODO: Change ExpiresAt it to LRU Bits as handled by Redis
 type Obj struct{
 	TypeEncoding uint8
-	Value        interface{}
-	ExpiresAt    int64
+	Value        interface{} //which means we acn put literally anything,anyvalue and which would work fine
+	// ExpiresAt    int64
+	/*
+	Earlier we use to have expireAt here, we don't need as we need LastAccessdAt
+	since golang doesn't support bitfield so LastAccessedAt is storing everytime the key is getting accessed
+	*/
+
+	/*
+	Redis allot 24 bits to these bits, but we will use 32 bits because
+	golang does not support bitfields and we need not make this super-compplicated 
+	by merging TypeEncoding + LastAccessedAt in one 32 bit integer
+	but nonthelss, we can benchmarks and see how that fares
+	For noe, we continue with 32 bit integer to stre th LastAccessedAt
+	*/
+	LastAccessedAt uint32
 }
 /*
 C and c++ give us ways to apply bit fields on set, means in each set i can assign a fix number of bits
