4d9263caa7a2ac359bde1243571e98369cb2affa
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-25 20:43:24 +0000

    feat: Simple sanity check for integration_test.go

---
+++ velox-benchmark/benchmark_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBenchmarkSanity(t *testing.T) {
+	// A simple sanity test for the benchmark package
+	t.Log("velox-benchmark package compiles successfully")
+}
