b0487992b18c122a20a4839bd0f641238fe2f8ba
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-04-25 20:25:29 +0000

    feat: Added globalCachedTime, to reduce expensive calls for time.Now().UnixMilli()

--- core/events.go
+++ core/events.go
@@ -1,6 +1,22 @@
 package core
 
-import "log"
+import (
+	"log"
+	"time"
+)
+
+// GlobalCachedTime holds the current time in Unix milliseconds.
+// It is updated once per event loop tick to eliminate thousands of expensive
+// time.Now().UnixMilli() system calls per second during high QPS loads.
+var GlobalCachedTime int64
+
+func init() {
+	GlobalCachedTime = time.Now().UnixMilli()
+}
+
+func UpdateCachedTime() {
+	GlobalCachedTime = time.Now().UnixMilli()
+}
 
 // InitStore replays the AOF file on startup to restore persisted data
 // Call this once before accepting any client connections
